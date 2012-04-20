f95b8bb5c98752058092e370727075cbf0b3bc4c
isaacs <i@izs.me>
2012-04-20 12:23:25 -0700

    middleware is dumb, just use a request handler

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-A tiny [tako](https://github.com/mikeal/tako) middleware
+A tiny [tako](https://github.com/mikeal/tako) request handler
 to bind to Jed Schmidt's [Cookies](https://github.com/jed/cookies) util.
--- cookies.js
+++ cookies.js
@@ -3,9 +3,6 @@ var Cookies = require("cookies")
 , tako = require("tako")
 , EE = require("events").EventEmitter
 
-var cookieMiddle = module.exports = new EE()
-tako.globalMiddles.cookies = tako.globalMiddles.cookies || cookieMiddle
-
-cookieMiddle.on("request", function (req, res) {
+module.exports = function (req, res) {
   req.cookies = res.cookies = new Cookies(req, res, keygrip)
-})
+}
