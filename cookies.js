39188dcd5cd9eca45764ddb2f9d0cdf60f14a19b
Felix Böhm <felixboehm55@googlemail.com>
2012-06-03 17:15:54 +0300

    call keygrip, removed useless dependencies

--- cookies.js
+++ cookies.js
@@ -1,7 +1,5 @@
 var Cookies = require("cookies")
-, keygrip = require("keygrip")
-, tako = require("tako")
-, EE = require("events").EventEmitter
+, keygrip = require("keygrip")()
 
 module.exports = function (req, res) {
   req.cookies = res.cookies = new Cookies(req, res, keygrip)
